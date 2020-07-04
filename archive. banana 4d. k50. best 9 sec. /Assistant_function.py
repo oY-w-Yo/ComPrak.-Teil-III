56f4284abcfd90eb2d50e2b2fcabdb49b130f192
oY-w-Yo <applewang17@hotmail.com>
2020-07-04 21:03:02 +0200

    .

---
+++ archive. banana 4d. k50. best 9 sec. /Assistant_function.py
@@ -0,0 +1,131 @@
+import numpy as np
+#import heapq as hp
+import math
+from   random import choice
+import timeit
+
+
+
+######################## Define some useful help functions ###########################
+
+# squre of distance of two pure points
+def distance_sq(p1,p2):
+    maxi = 0
+    for a,b in zip(p1,p2):
+        c = abs(a-b)
+        if c > maxi:
+            maxi = c
+    return maxi
+
+
+def distance_sq2(p1,p2):
+    maxi = 0
+    for i in range(len(p1)):
+        c = p1[i] - p2[i]
+        if c > maxi:
+            maxi = c
+    return maxi
+
+'''
+test1 = 'p1 = [10,203,4,55,40];p2 = [20,40,120,40,1];distance_sq(p1,p2)'
+test2 = 'p1 = [10,203,4,55,40];p2 = [20,40,120,40,1];distance_sq2(p1,p2)'
+print(timeit.timeit(test1,'from __main__ import distance_sq',number=100000))
+print(timeit.timeit(test2,'from __main__ import distance_sq2',number=100000))
+'''
+
+# d(p,Ball) = d(p,Ball_center) - Ball_radius
+# d(p,Ball)^2 = d(p,Ball_center)^2 + Ball_radius^2 - 2*d(p,Ball_center)*Ball_radius, also
+# d(p,Ball)_sq = d(p,Ball_center)_sq + Ball_radius_sq 
+def distance_sq_Ball(p,Ball_center,Ball_radius_sq):
+    k_sq = distance_sq(p,Ball_center)
+    r_sq = Ball_radius_sq 
+    d_sq = k_sq + r_sq - 2*math.sqrt(k_sq * r_sq)
+    return d_sq
+ 
+# special for the datastructure, we have data in form [label, vektor] in pSet
+def k_closest_point(Point,pSet,k):
+    index_list = range(len(pSet)) 
+    distance_set = [distance_sq(p[1],Point) for p in pSet]
+    index_list = sorted(index_list, key = lambda i: distance_set[i])[:k]
+    #index_list = hp.nsmallest(k,index_list, key = lambda i: distance_set[i])
+    return [pSet[i] for i in index_list],[distance_set[i] for i in index_list]
+
+# p is a pure vektor and every point in  pSet is in form [label, vektor]
+# the output is also in form [label, vektor]
+def farthest_point(p,pSet):
+    farthest = pSet[0]
+    for q in pSet:
+        if distance_sq(p,q[1]) > distance_sq(p,farthest[1]):
+            farthest = q
+    return farthest
+
+# 1. choose a point p randomly 2. find the point f1, fartherst from p
+# 3. find the point f2, fartherst from f1
+def greatest_spread(pSet):
+    p = choice(pSet)
+    f1 = farthest_point(p[1],pSet)
+    f2 = farthest_point(f1[1],pSet)
+    f = [f1[1][i] - f2[1][i] for i in range(len(f1[1]))]
+    return f
+
+
+def projection(w,p):
+    return np.dot(w,p) # this function is here to make the code more readable
+
+
+def ErrorCal(func,TestSet):
+    m = len(TestSet)
+    Error = sum(labeled_point[0] != func(labeled_point[1]) for labeled_point in TestSet)
+    Error = Error/m
+    return Error
+
+def Error(Point,k_best):
+    result = sum(p[0] for p in k_best)
+    if result < 0:
+        result = -1
+    else:
+        result = 1
+    return result != Point[0]
+
+def point_error(Point,sum_label):
+    if sum_label < 0:
+        result = -1
+    else:
+        result = 1
+    return result != Point[0]
+
+def merge_two_k_best(Set1,distance_set1,Set2,distance_set2,k):
+    if Set1 == []:
+        return Set2,distance_set2
+    #if Set2 == []:
+    #    return Set1,distance_set1
+    if len(Set1) < k or distance_set2[0] < distance_set1[-1]:
+        temp_Best = Set1 + Set2
+        temp_distance_set = distance_set1 + distance_set2
+        temp_index = range(len(temp_Best))
+        temp_index = sorted(temp_index, key = lambda i: temp_distance_set[i])[:k]
+        Set1 = [temp_Best[i] for i in temp_index]
+        distance_set1 = [temp_distance_set[i] for i in temp_index]
+    return Set1,distance_set1
+
+def sgnial(value):
+    if value < 0:
+        return -1
+    else:
+        return 1
+
+def axis_find(pSet,dimension):
+    pSet_sort = []
+    width = 0
+    d = 0
+    for i in range(dimension):
+        pSet_temp_sort = sorted(pSet,key=lambda p: p[1][i])
+        width_temp = pSet_temp_sort[-1][1][i] - pSet_temp_sort[0][1][i]
+        if width_temp > width:
+            width = width_temp
+            pSet_sort = pSet_temp_sort
+            d = i
+    
+    return pSet_sort,d
+
+    
---
+++ archive. banana 4d. k50. best 9 sec. /K_Nearest_Neighbour_kdtree.py
@@ -0,0 +1,158 @@
+from random import shuffle
+from read_and_write import read_csv
+import numpy as np
+import Assistant_class as AC
+import Assistant_function as AF
+import time
+import copy
+
+class Kd_Node:
+    def __init__(self, Data, depth = 0):
+        #self.position = position
+        self.depth = depth
+        self.axis = 0
+
+        self.Data = Data
+        self.pivot = None
+
+        self.LeftChild = None
+        self.RightChild = None
+        #self.oppesite = None
+        
+        self.create_kd_node()
+    
+    def create_kd_node(self):
+        n = len(self.Data)
+        if n <= Leafsize:
+            self.position = AC.Position.Leaf
+        else:
+            self.position = AC.Position.InsidePoint
+            sorted_points,self.axis = AF.axis_find(self.Data,dimension)
+            #axis = self.depth % dimension
+            #sorted_points = sorted(self.Data, key = lambda point: point[1][axis])
+            self.pivot = sorted_points[int(n/2)]
+            self.LeftChild  = Kd_Node(sorted_points[:int(n/2)], depth = self.depth+1)
+            self.RightChild = Kd_Node(sorted_points[int(n/2):], depth = self.depth+1)
+            #self.LeftChild.oppesite = self.RightChild
+            #self.RightChild.oppesite = self.LeftChild
+
+    def stacking_from_kd_node(self,Point):
+        Stack = []
+        current_node = self
+        while current_node.position != AC.Position.Leaf:
+            axis = current_node.axis
+            #axis = current_node.depth % dimension
+            if Point[axis] < current_node.pivot[1][axis]:
+                current_node = current_node.LeftChild
+            else:
+                current_node = current_node.RightChild
+            Stack.append(current_node)
+        return Stack
+
+    def search_k_nearst_from_kd_node(self,Point,k):
+        Stack = [self]
+        Stack.extend(self.stacking_from_kd_node(Point))
+        k_Best = []
+        distance_set = []
+        while Stack != []:
+            current_node = Stack.pop()
+            if current_node.position == AC.Position.Leaf:
+                local_k_best,local_distance_set = AF.k_closest_point(Point,current_node.Data,k)
+                k_Best,distance_set = AF.merge_two_k_best(k_Best,distance_set,local_k_best,local_distance_set,k)
+            else:
+                axis = current_node.axis
+                #axis = current_node.depth % dimension
+                if len(k_Best) < k or distance_set[-1] > abs(Point[axis] - current_node.pivot[1][axis]):
+                    Stack.extend(current_node.stacking_from_kd_node(Point))
+        return k_Best,distance_set
+
+# Define f_D_k function
+def classify_kd (name,KSET,l):
+
+    k_star = None
+    k_max = max(KSET)
+
+    global Leafsize 
+    Leafsize = k_max*0.9
+
+    # read the data
+    trainSet = read_csv(name,"train")
+
+    # set dimension
+    global dimension
+    dimension = len(trainSet[0][1])
+
+    # randomly divide the data
+    shuffle(trainSet)
+    m = int(len(trainSet)/l)
+    divided_trainSet = []
+    for i in range(0,len(trainSet),m):
+        divided_trainSet.append(trainSet[i:i+m])
+
+    # build the tree 
+    tree_root_list_with_i = []
+    k_max_best = [] # in form [[Point_in_i,k_max_best_in_without_i,sum_label],...,]
+    for i in range(l):
+        local_trainSet = [x for j in range(l) for x in divided_trainSet[j] if j != i]
+        start2 = time.time()
+        root_node_without_i     = Kd_Node(local_trainSet) 
+        root_node_with_i        = Kd_Node(divided_trainSet[i]) 
+        print("l={},tree_build".format(i),time.time()-start2)
+        tree_root_list_with_i.append(root_node_with_i)
+        # search the max_k nearst Element of each local_train_set and store them in the list
+        start2 = time.time()
+        for p in divided_trainSet[i]:
+            k_max_best_in_without_i,_ = root_node_without_i.search_k_nearst_from_kd_node(p[1],k_max)
+            # add a new list to store the accumulated sum of label 
+            sum_label = []
+            sum_label.append(k_max_best_in_without_i[0][0])
+            for j in range(1,len(k_max_best_in_without_i)):
+                sum_label.append(sum_label[j-1] + k_max_best_in_without_i[j][0])
+            k_max_best.append([p,k_max_best_in_without_i,sum_label])
+        print("l={},tree_search".format(i),time.time()-start2)
+    
+    # for every k in KSET, evaluate the error and find the best k_star
+    min_Error = 1
+    for k in KSET:
+        #average_Error = np.mean([AF.Error(p[0],p[1][:k]) for p in k_max_best])
+        average_Error = np.mean([AF.point_error(p[0],p[2][k-1]) for p in k_max_best])
+        #print(average_Error)
+        if average_Error < min_Error:
+            min_Error = average_Error
+            k_star = k
+
+    def f_D_k_result(Point):
+        k_star_best = [] # in form[[Label,Vector,distance_from_Point,i],...,] <- len = k_star
+        #k_star_result = []
+        for i in range(l):
+            k_star_best_in_i,distance_set_in_i = tree_root_list_with_i[i].search_k_nearst_from_kd_node(Point,k_star)
+            for m in range(k_star):
+                k_star_best_in_i[m].append(distance_set_in_i[m]) 
+                k_star_best_in_i[m].append(i) 
+            k_star_best.extend(k_star_best_in_i)
+        
+        k_star_best = sorted(k_star_best, key = lambda p: p[2])
+        #print(Point,k_star_best)
+
+        temp_summ = 0
+        summ = 0
+        for i in range(l):
+            count = 0
+            for p in k_star_best:
+                if p[3] != i:
+                    temp_summ += p[0]
+                    count += 1 
+                if count >= k:
+                    break
+            if temp_summ < 0:
+                summ -= 1
+            else:
+                summ += 1
+
+        if summ < 0:
+            return -1
+        else:
+            return 1
+
+    return [k_star,f_D_k_result]
+    
